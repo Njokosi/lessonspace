d04455885a6ccc41a6f836dbc8fd75ec63dc092f
Eng.Njokosi <eng.njokosi@gmail.com>
2021-08-03 17:48:58 +0300

    update fix logo

--- components/nav.js
+++ components/nav.js
@@ -110,30 +110,17 @@ export default function Nav() {
                 <div className="flex items-center justify-between mb-4">
                   <div>
                     <a
-                      href="/"
-                      aria-label="Company"
-                      title="Company"
-                      className="inline-flex items-center"
+                      class="inline-flex items-center"
+                      aria-label="Lessonspace"
+                      title="Lessonspace"
                     >
-                      <svg
-                        className="w-8 text-indigo-400"
-                        viewBox="0 0 24 24"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeMiterlimit="10"
-                        stroke="currentColor"
-                        fill="none"
-                      >
-                        <rect x="3" y="1" width="7" height="12" />
-                        <rect x="3" y="17" width="7" height="6" />
-                        <rect x="14" y="1" width="7" height="6" />
-                        <rect x="14" y="11" width="7" height="12" />
-                      </svg>
-                      <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
-                        Company
-                      </span>
+                      <img
+                        class="h-8"
+                        src="/images/lessonspacelogo.svg"
+                        alt="Lessonspace"
+                      />
                     </a>
+                    
                   </div>
                   <div>
                     <button
